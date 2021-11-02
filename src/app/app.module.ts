46f697c88c9930c1e2ee6c6910d478ca54051308
Matias Gonzalez <mng_epson@hotmail.com>
2021-11-01 21:08:23 -0300

    Bienvenida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { BienvenidaComponent } from './components/inicio/bienvenida/bienvenida.component';
+import { LoginComponent } from './components/inicio/login/login.component';
+import { RegisterComponent } from './components/inicio/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    BienvenidaComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
